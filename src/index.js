6c10e82a862a3018e7c1aae2daccabe83d75e6ec
Jori <jvalkenhoff@outlook.com>
2022-02-14 21:07:46 +0100

    added react router, created folders for api, components, routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import { 
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+
+render(
   <React.StrictMode>
-    <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
+  </BrowserRouter>,
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
